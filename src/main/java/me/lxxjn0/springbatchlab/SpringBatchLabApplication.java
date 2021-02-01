41e9f0f5c5a4cc833c37f09dd9c4ddbf9c25385d
lxxjn0 <ljy3510@gmail.com>
2021-02-01 13:03:08 +0900

    feat(init): initial commit

---
+++ src/main/java/me/lxxjn0/springbatchlab/SpringBatchLabApplication.java
@@ -0,0 +1,13 @@
+package me.lxxjn0.springbatchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/lxxjn0/springbatchlab/SpringBatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lxxjn0.springbatchlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
